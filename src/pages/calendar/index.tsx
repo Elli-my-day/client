8594a8f169169ea490167381b0d710785bfe3f20
raverena <raverena96@gmail.com>
2022-10-19 15:14:07 +0900

    [Add] calendar event

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
+import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
 const Calendar = () => {
+  const calRef = useRef(null);
+
+  const _events = [
+    { title: 'event 1', start: '2022-10-01', end: '2022-10-03' },
+    { title: 'event 2', start: '2022-10-04', end: '2022-10-06' },
+  ];
+
+  const [calendarEvents, setEvents] = useState(_events);
+
   return (
     <>
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      <FullCalendar
+        ref={calRef}
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        selectable
+        editable
+        droppable
+        events={calendarEvents}
+        select={(arg) => {
+          console.log(arg);
+          if (calRef.current) {
+            const cal = calRef.current;
+          }
+        }}
+      />
     </>
   );
 };
